a0b2d9e6b6ef042d0b5620ddea3c6cea3bf8cb05
Hoseus <rodrigo.llopis@redb.ee>
2019-12-27 05:42:19 -0300

    Changes in poll message and a better handling of options and results

--- src/main.rs
+++ src/main.rs
@@ -7,100 +7,122 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use serenity::model::id::{GuildId, UserId, ChannelId};
+use serenity::model::id::{GuildId};
 use serenity::model::voice::VoiceState;
 use serenity::Client as SerenityClient;
 use reqwest::Client as ReqwestClient;
 use serenity::http::CacheHttp;
-use serenity::model::user::User;
-use serenity::model::guild::PartialGuild;
 
 struct Handler;
 
+const DISCORD_BOT_TOKEN_ENV: &str = "DISCORD_BOT_TOKEN";
+const TELEGRAM_BOT_TOKEN_ENV: &str = "TELEGRAM_BOT_TOKEN";
+const TELEGRAM_CHAT_ID_ENV: &str = "TELEGRAM_CHAT_ID";
+
 const TELEGRAM_API_URL: &str = "https://api.telegram.org";
+const TELEGRAM_POLL_OPTIONS: &str = "[\"Yes\",\"No\"]";
+
+const NOT_OBTAINED_STRING: &str = "<not_obtained>";
 
 lazy_static! {
-    static ref DISCORD_BOT_TOKEN: String = env::var("DISCORD_BOT_TOKEN").unwrap();
-    static ref TELEGRAM_BOT_TOKEN: String = env::var("TELEGRAM_BOT_TOKEN").unwrap();
-    static ref TELEGRAM_CHAT_ID: String = env::var("TELEGRAM_CHAT_ID").unwrap();
-    static ref REQWEST_CLIENT: reqwest::Client = ReqwestClient::new();
-}
+    static ref DISCORD_BOT_TOKEN: String = env::var(DISCORD_BOT_TOKEN_ENV).expect(format!("Env variable not defined: {}", DISCORD_BOT_TOKEN_ENV).clone().as_str());
+    static ref TELEGRAM_BOT_TOKEN: String = env::var(TELEGRAM_BOT_TOKEN_ENV).expect(format!("Env variable not defined: {}", TELEGRAM_BOT_TOKEN_ENV).clone().as_str());
+    static ref TELEGRAM_CHAT_ID: String = env::var(TELEGRAM_CHAT_ID_ENV).expect(format!("Env variable not defined: {}", TELEGRAM_CHAT_ID_ENV).clone().as_str());
 
-impl EventHandler for Handler {
-
-
-
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "-ring" {
-            let user: User = msg.author;
-            let user_name: &str = user.name.as_str();
-
-            let channel_id: ChannelId = msg.channel_id;
-            let channel_name: String = channel_id.name(ctx.cache().unwrap()).unwrap();
-
-            let guild_id: GuildId = msg.guild_id.unwrap();
-            let guild: PartialGuild = guild_id.to_partial_guild(ctx.http()).unwrap();
-            let guild_name: String = guild.name;
-
-            let message: String = format!("Ring! *{}* is calling in text channel *{}* in server *{}*", user_name, channel_name, guild_name);
-
-            send_message_to_telegram(message.as_str());
-
-        }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-
-    fn voice_state_update(&self, _ctx: Context, _guild_id: Option<GuildId>, _old_voice_state: Option<VoiceState>, new_voice_state: VoiceState) {
-        let user_id: UserId = new_voice_state.user_id;
-        let user: User = user_id.to_user(_ctx.http()).unwrap();
-        let user_name: &str = user.name.as_str();
-
-        let channel_id: ChannelId = new_voice_state.channel_id.unwrap();
-        let channel_name: String = channel_id.name(_ctx.cache().unwrap()).unwrap();
-
-        let guild_id: GuildId = _guild_id.unwrap();
-        let guild: PartialGuild = guild_id.to_partial_guild(_ctx.http()).unwrap();
-        let guild_name: String = guild.name;
-
-        let message: String = format!("Ring! *{}* joined to voice channel *{}* in server *{}*", user_name, channel_name, guild_name);
-
-        send_message_to_telegram(message.as_str());
-    }
+    static ref REQWEST_CLIENT: ReqwestClient = ReqwestClient::new();
 }
 
 fn main() {
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut serenity_client = SerenityClient::new(DISCORD_BOT_TOKEN.as_str(), Handler).expect("Error creating client");
+    let mut serenity_client = SerenityClient::new(DISCORD_BOT_TOKEN.as_str(), Handler).expect("Error creating Serenity client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = serenity_client.start() {
         println!("Serenity client error: {:?}", why);
     }
 }
 
-fn send_message_to_telegram(message: &str) {
-    let url: String = format!("{}/bot{}/sendMessage?parse_mode=Markdown&chat_id={}&text={}", TELEGRAM_API_URL, *TELEGRAM_BOT_TOKEN, *TELEGRAM_CHAT_ID, message);
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "-ring" {
+            /*let user: User = msg.author;
+            let user_name: String = user.name;*/
+            let user_name: String = msg.author.name;
 
-    println!("Sending to telegram: {}", url);
+            /*let channel_id: ChannelId = msg.channel_id;
+            let channel_name: String = channel_id.name(ctx.cache().unwrap()).unwrap_or_else("<not_obtained>".to_string());*/
 
-    let _response = REQWEST_CLIENT.get(url.as_str()).send();
+            let channel_name: String = msg.channel_id.name(ctx.cache().unwrap()).unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+            /*let guild_id: GuildId = msg.guild_id;
+            let guild: PartialGuild = guild_id.to_partial_guild(ctx.http()).unwrap();
+            let guild_name: String = guild.name;*/
+
+            let guild_name: String =
+                msg.guild_id
+                    .map(|guild_id| guild_id.to_partial_guild(ctx.http())).map(|partial_guild_result| partial_guild_result.ok()).flatten()
+                    .map(|partial_guild| partial_guild.name)
+                    .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+            send_poll_to_telegram(user_name, channel_name, guild_name);
+
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} connected to discord server successfully", ready.user.name);
+    }
+
+    fn voice_state_update(&self, _ctx: Context, _guild_id: Option<GuildId>, _old_voice_state: Option<VoiceState>, new_voice_state: VoiceState) {
+        /*let user_id: UserId = new_voice_state.user_id;
+        let user: User = user_id.to_user(_ctx.http()).unwrap();
+        let user_name: &str = user.name.as_str();*/
+
+        let user_name: String =
+            new_voice_state.user_id
+                .to_user(_ctx.http())
+                .map(|user| user.name)
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        /*let channel_id: ChannelId = new_voice_state.channel_id.unwrap();
+        let channel_name: String = channel_id.name(_ctx.cache().unwrap()).unwrap();*/
+
+        let channel_name: String =
+            new_voice_state.channel_id
+                .map(|channel_id| channel_id.name(_ctx.cache().unwrap()))
+                .flatten()
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        /*let guild_id: GuildId = _guild_id.unwrap();
+        let guild: PartialGuild = guild_id.to_partial_guild(_ctx.http()).unwrap();
+        let guild_name: String = guild.name;*/
+
+        let guild_name: String =
+            _guild_id
+                .map(|guild_id| guild_id.to_partial_guild(_ctx.http()))
+                .map(|partial_guild_result| partial_guild_result.ok())
+                .flatten()
+                .map(|partial_guild| partial_guild.name)
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        send_poll_to_telegram(user_name, channel_name, guild_name);
+    }
+}
+
+fn send_message_to_telegram(user_name: String, channel_name: String, guild_name: String) {
+    let message: String = format!("Ring! *{}* joined to voice channel *{}* in server *{}*", user_name, channel_name, guild_name);
+
+    let url_string: String = format!("{}/bot{}/sendMessage?parse_mode=Markdown&chat_id={}&text={}", TELEGRAM_API_URL, *TELEGRAM_BOT_TOKEN, *TELEGRAM_CHAT_ID, message);
+
+    println!("Sending to telegram: {}", url_string);
+
+    let _response = REQWEST_CLIENT.get(url_string.as_str()).send();
+}
+
+fn send_poll_to_telegram(user_name: String, channel_name: String, guild_name: String) {
+    let message: String = format!("Ring! *{}* joined to voice channel *{}* in server *{}*. Are you joining?", user_name, channel_name, guild_name);
+
+    let url_string: String = format!("{}/bot{}/sendPoll?chat_id={}&question={}&options={}", TELEGRAM_API_URL, *TELEGRAM_BOT_TOKEN, *TELEGRAM_CHAT_ID, message, TELEGRAM_POLL_OPTIONS);
+
+    println!("Sending to telegram: {}", url_string);
+
+    let _response = REQWEST_CLIENT.get(url_string.as_str()).send();
 }
