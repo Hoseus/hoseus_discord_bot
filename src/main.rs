b2ed463839c8d51382a86a295db849fddbba23ae
Hoseus <rodrigo.llopis@redb.ee>
2019-12-27 11:05:09 -0300

    Add ttl for poll

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,14 @@ use serenity::model::voice::VoiceState;
 use serenity::Client as SerenityClient;
 use reqwest::Client as ReqwestClient;
 use serenity::http::CacheHttp;
+use chrono::{DateTime, Utc, Duration};
 
 struct Handler;
 
 const DISCORD_BOT_TOKEN_ENV: &str = "DISCORD_BOT_TOKEN";
 const TELEGRAM_BOT_TOKEN_ENV: &str = "TELEGRAM_BOT_TOKEN";
 const TELEGRAM_CHAT_ID_ENV: &str = "TELEGRAM_CHAT_ID";
+const TELEGRAM_POLL_TTL_ENV: &str = "TELEGRAM_POLL_TTL";
 
 const TELEGRAM_API_URL: &str = "https://api.telegram.org";
 const TELEGRAM_POLL_OPTIONS: &str = "[\"Yes\",\"No\"]";
@@ -26,11 +28,15 @@ const NOT_OBTAINED_STRING: &str = "<not_obtained>";
 
 lazy_static! {
     static ref DISCORD_BOT_TOKEN: String = env::var(DISCORD_BOT_TOKEN_ENV).expect(format!("Env variable not defined: {}", DISCORD_BOT_TOKEN_ENV).clone().as_str());
+
     static ref TELEGRAM_BOT_TOKEN: String = env::var(TELEGRAM_BOT_TOKEN_ENV).expect(format!("Env variable not defined: {}", TELEGRAM_BOT_TOKEN_ENV).clone().as_str());
     static ref TELEGRAM_CHAT_ID: String = env::var(TELEGRAM_CHAT_ID_ENV).expect(format!("Env variable not defined: {}", TELEGRAM_CHAT_ID_ENV).clone().as_str());
+    static ref TELEGRAM_POLL_TTL: Duration = Duration::seconds(env::var(TELEGRAM_POLL_TTL_ENV).expect(format!("Env variable not defined: {}", TELEGRAM_POLL_TTL_ENV).clone().as_str()).parse::<i64>().unwrap());
 
     static ref REQWEST_CLIENT: ReqwestClient = ReqwestClient::new();
 }
+
+static mut POLL_EXPIRATION: Option<DateTime<Utc>> = None;
 
 fn main() {
     let mut serenity_client = SerenityClient::new(DISCORD_BOT_TOKEN.as_str(), Handler).expect("Error creating Serenity client");
@@ -54,7 +60,6 @@ impl EventHandler for Handler {
                     .unwrap_or(NOT_OBTAINED_STRING.to_string());
 
             send_text_channel_poll_to_telegram(user_name, channel_name, guild_name);
-
         }
     }
 
@@ -71,27 +76,50 @@ impl EventHandler for Handler {
 }
 
 fn voice_state_join_channel(_handler: &Handler, _ctx: Context, _guild_id: Option<GuildId>, _old_voice_state: Option<VoiceState>, new_voice_state: VoiceState) {
-    let user_name: String =
-        new_voice_state.user_id
-            .to_user(_ctx.http())
-            .map(|user| user.name)
-            .unwrap_or(NOT_OBTAINED_STRING.to_string());
+    let now: DateTime<Utc> = Utc::now();
 
-    let channel_name: String =
-        new_voice_state.channel_id
-            .map(|channel_id| channel_id.name(_ctx.cache().unwrap()))
-            .flatten()
-            .unwrap_or(NOT_OBTAINED_STRING.to_string());
+    println!("poll_ttl: {}", *TELEGRAM_POLL_TTL);
+    println!("poll_ttl2: {}", Duration::seconds(600));
+    println!("now: {}", now);
 
-    let guild_name: String =
-        _guild_id
-            .map(|guild_id| guild_id.to_partial_guild(_ctx.http()))
-            .map(|partial_guild_result| partial_guild_result.ok())
-            .flatten()
-            .map(|partial_guild| partial_guild.name)
-            .unwrap_or(NOT_OBTAINED_STRING.to_string());
+    unsafe {
+        match POLL_EXPIRATION {
+            Some(p) => println!("poll_expiration: {}", p),
+            _ => ()
+        }
+    };
 
-    send_voice_channel_poll_to_telegram(user_name, channel_name, guild_name);
+    if unsafe { POLL_EXPIRATION.is_none() || (POLL_EXPIRATION.is_some() && now > POLL_EXPIRATION.unwrap()) } {
+        let user_name: String =
+            new_voice_state.user_id
+                .to_user(_ctx.http())
+                .map(|user| user.name)
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        let channel_name: String =
+            new_voice_state.channel_id
+                .map(|channel_id| channel_id.name(_ctx.cache().unwrap()))
+                .flatten()
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        let guild_name: String =
+            _guild_id
+                .map(|guild_id| guild_id.to_partial_guild(_ctx.http()))
+                .map(|partial_guild_result| partial_guild_result.ok())
+                .flatten()
+                .map(|partial_guild| partial_guild.name)
+                .unwrap_or(NOT_OBTAINED_STRING.to_string());
+
+        send_voice_channel_poll_to_telegram(user_name, channel_name, guild_name);
+
+        unsafe {
+            POLL_EXPIRATION.replace(now + *TELEGRAM_POLL_TTL);
+            match POLL_EXPIRATION {
+                Some(p) => println!("poll_expiration: {}", p),
+                _ => ()
+            }
+        };
+    }
 }
 
 fn send_voice_channel_poll_to_telegram(user_name: String, channel_name: String, guild_name: String) {
